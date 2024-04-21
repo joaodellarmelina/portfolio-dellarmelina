6620c296d60d427d4f8979c604fbdec84fd52c95
João Vitor Dellarmelina <125659911+joaodellarmelina@users.noreply.github.com>
2024-04-20 23:16:50 -0300

    new header style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,18 +14,27 @@ const Header = () => {
 
    return (
     <header className="w-full h-24"> 
-      <div className="h-12 w-full absolute -z-1 hidden lg:block lg:blur-xl lg:bg-neutral-700"></div>
+{/* PC SCREEN */}
+      <div className="rounded-full h-12 
+      w-[70%] left-[15%] xl:w-[60%] xl:left-[20%] top-8 absolute -z-1 hidden 
+      lg:block lg:blur-xl lg:bg-neutral-700"></div>
  
-      <nav id="open-menu" className="absolute hidden rounded-bl-full h-48 w-full bg-neutral-800 overflow-auto">
-        <ul className="flex gap-6 py-8 text-left justify-center items-center flex-col z-20">
-          <Link href={'/'}><li className="text-neutral-400 line-through  active:text-neutral-500 text-md font-bold">Sobre mim</li></Link>
-          <Link href={'/'}><li className="text-neutral-400 active:text-neutral-500 text-md font-bold">Experiências</li></Link>
-          <Link href={'/'}><li className="text-neutral-400 active:text-neutral-500 text-md font-bold">Projetos</li></Link>
-        </ul>
+      <div className="absolute right-10 top-10 gap-4 hidden lg:flex">
+        <Link href={'/'}><Image src={'/languages/flag-brazil.png'} alt={''} width={24} height={24} /></Link>
+        <a onClick={NotYet} ><Image src={'/languages/flag-us.png'} className="cursor-pointer" alt={''} width={24} height={24} /></a>
+      </div>
+
+      <nav className="lg:flex hidden justify-center gap-56 relative top-10
+      text-neutral-500 font-bold text-lg">
+        <Link href="/" className="relative hover:bottom-1 hover:brightness-150">Sobre mim</Link>
+        <Link href="/" className="relative hover:bottom-1 hover:brightness-150">Experiências</Link>
+        <Link href="/" className="relative hover:bottom-1 hover:brightness-150">Projetos</Link>
       </nav>
+ 
 
+{/* CELULAR SCREEN */}
 
-      <div className="sm:hidden block p-6 z-20 absolute right-5 ">
+      <div className="lg:hidden block p-6 z-20 absolute right-5 ">
         <Hamburger color="#8c8c8c" rounded label="Show menu" hideOutline={false} easing="ease-in"  onToggle={toggled => {
           let menu = document.getElementById('open-menu');
           let html = document.getElementsByTagName('html')[0];
@@ -38,18 +47,14 @@ const Header = () => {
           }
           }} />
       </div>
-      
-      <div className="absolute right-10 top-5 gap-4 hidden  sm:flex">
-        <Link href={'/'}><Image src={'/languages/flag-brazil.png'} alt={''} width={24} height={24} /></Link>
-        <a onClick={NotYet} ><Image src={'/languages/flag-us.png'} className="cursor-pointer" alt={''} width={24} height={24} /></a>
-      </div>
-
-      <nav className="lg:flex hidden justify-center gap-56 relative top-10
-      text-neutral-500 font-bold text-lg">
-        <Link href="/" className="relative hover:bottom-1 hover:blur-lg hover:brightness-150 line-through">Sobre mim</Link>
-        <Link href="/" className="relative hover:bottom-1 hover:brightness-150">Experiências</Link>
-        <Link href="/" className="relative hover:bottom-1 hover:brightness-150">Projetos</Link>
+      <nav id="open-menu" className="absolute hidden rounded-bl-full h-48 w-full bg-neutral-800 overflow-auto">
+        <ul className="flex gap-6 py-8 text-left justify-center items-center flex-col z-20">
+          <Link href={'/'}><li className="text-neutral-400 active:text-neutral-500 text-md font-bold">Sobre mim</li></Link>
+          <Link href={'/'}><li className="text-neutral-400 active:text-neutral-500 text-md font-bold">Experiências</li></Link>
+          <Link href={'/'}><li className="text-neutral-400 active:text-neutral-500 text-md font-bold">Projetos</li></Link>
+        </ul>
       </nav>
+      
 
     </header>
   )
