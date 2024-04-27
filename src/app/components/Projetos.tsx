af1fdb2b00b340a69124a76f129c5f5ba402ef26
João Vitor Dellarmelina <125659911+joaodellarmelina@users.noreply.github.com>
2024-04-27 01:34:52 -0300

    new project page

--- src/app/components/Projetos.tsx
+++ src/app/components/Projetos.tsx
@@ -1,180 +1,101 @@
-'use client'
-import Image from 'next/image'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import Link from 'next/link'
-import { useState } from 'react'
-import swal from 'sweetalert'
+import { Badge } from '@/components/ui/badge'
 
-type Projects = 'parkspace-lp' | 'parkspace-app' | 'mazzi' | 'carnawash'
+type Project = {
+  name: string
+  github: string
+  site?: string
+  description: string
+}
 
-const getClassNameForProject = (
-  currentProject: Projects | null,
-  project: Projects
-) => (currentProject === project ? 'flex flex-col' : 'hidden')
+const projects: Project[] = [
+  {
+    name: 'Carnawash | Site',
+    site: 'https://www.carnawashapp.com/',
+    description:
+      'Site feito para um startup localizada na autralia, feito em Next.js e TailwindCSS.',
+    github: '',
+  },
+  {
+    name: 'Parkspace | Landing Page',
+    site: 'https://parkspace.app/',
+    description:
+      'Desenvolvimento do site da startup Parkspace, localizada na Espanha, o site foi feito usando HTML e TailwindCSS.',
+    github: '',
+  },
+  {
+    name: 'YouDriver | Landing Page',
+    site: 'https://youdriver-lp.vercel.app/',
+    description: 'Landing Page para startup.',
+    github: '',
+  },
+]
 
-const Projetos = () => {
-  const [currentProject, setCurrentProject] = useState<Projects | null>(null)
-
-  const NotYet = () => {
-    swal({
-      title: 'oooh no! :(',
-      text: 'Esse aplicativo ainda está em desenvolvimento...',
-    })
-  }
-
+const ProjetosTitle = () => {
   return (
-    <section
-      id="projetosSec"
-      className="hidden flex gap-12 flex-col justify-center items-center"
-    >
+    <>
       <h1
         className="font-extrabold 
 			text-3xl
 			md:text-5xl 
 			[text-wrap:balance] 
 			bg-clip-text 
-			text-transparent 
-			bg-gradient-to-r from-neutral-200/60 to-50% to-neutral-200"
+			text-neutral-200"
       >
         Projetos
       </h1>
 
       <p
-        className="px-6 font-normal text-md lg:text-xl text-neutral-300 text-center 
+        className="px-6 font-normal text-sm lg:text-lg text-neutral-300 text-center 
 			[text-wrap:balance]"
       >
         Alguns dos projetos que desenvolvi ou participei, com foco em Front-end
         e UI/UX Design.
-      </p>
-      <p
-        className="px-2 font-normal text-md lg:text-xl text-neutral-300 text-center 
-			[text-wrap:balance]"
-      >
+        <br />
+        <br />
         Clique nas imagens para saber mais sobre cada projeto:
       </p>
-      <article className="md:grid md:grid-cols-3 gap-2 md:gap-12 grid grid-cols-1 md:px-0 px-4">
-        {/* Carnawash */}
-        <div className="relative flex flex-col items-center gap-6">
-          <a onClick={() => setCurrentProject('carnawash')}>
-            <Image
-              className="rounded-lg cursor-pointer"
-              src={'/projetos/Carnawash.svg'}
-              alt="parkspace"
-              width={416}
-              height={60}
-            />
-          </a>
+    </>
+  )
+}
 
-          <div
-            id="InfoCarnawashSite"
-            className={getClassNameForProject(currentProject, 'carnawash')}
-          >
-            <div className="flex flex-col gap-1 items-center">
-              <Link href={'https://www.carnawashapp.com/'} target="_blank">
-                <h1 className="font-bold text-md underline">Carnawash</h1>
-              </Link>
-              <h2 className="font-light text-sm">Site</h2>
-            </div>
-
-            <div className="flex gap-6 p-4">
-              <Image
-                src={'/programacao/Nextjs.svg'}
-                alt="Next Js"
-                width={36}
-                height={24}
-              />
-              <Image
-                src={'/programacao/Tailwind.svg'}
-                alt="Tailwind CSS"
-                width={36}
-                height={24}
-              />
-            </div>
-          </div>
-        </div>
-        {/* Parkspace Site */}
-        <div className="relative flex flex-col items-center gap-6">
-          <a onClick={() => setCurrentProject('parkspace-lp')}>
-            <Image
-              className="rounded-lg cursor-pointer"
-              src={'/projetos/parkspace.svg'}
-              alt="parkspace"
-              width={400}
-              height={60}
-            />
-          </a>
-
-          <div
-            id="InfoParkspaceSite"
-            className={getClassNameForProject(currentProject, 'parkspace-lp')}
-          >
-            <div className="flex flex-col gap-1 items-center">
-              <Link href={'https://parkspace.app'} target="_blank">
-                <h1 className="font-bold text-md underline">Parkspace Site</h1>
-              </Link>
-              <h2 className="font-light text-sm">Landing Page</h2>
-            </div>
-
-            <div className="flex gap-6 p-4">
-              <Image
-                src={'/programacao/html.svg'}
-                alt="HTML"
-                width={36}
-                height={24}
-              />
-              <Image
-                src={'/programacao/Tailwind.svg'}
-                alt="Tailwind CSS"
-                width={36}
-                height={24}
-              />
-              <Image
-                src={'/programacao/javascript.svg'}
-                alt="JavaScript"
-                width={36}
-                height={24}
-              />
-            </div>
-          </div>
-        </div>
-        {/* Mazzi */}
-        <div className="relative flex flex-col items-center gap-6">
-          <a onClick={() => setCurrentProject('mazzi')}>
-            <Image
-              className="rounded-lg cursor-pointer"
-              src={'/projetos/YouDriver.svg'}
-              alt="parkspace"
-              width={400}
-              height={60}
-            />
-          </a>
-
-          <div
-            id="InfoYouDriver"
-            className={getClassNameForProject(currentProject, 'mazzi')}
-          >
-            <div className="flex flex-col gap-1 items-center">
-              <Link href={'https://www.youdriver.com.br/'} target="_blank">
-                <h1 className="font-bold text-md underline">You Driver</h1>
-              </Link>
-              <h2 className="font-light text-sm">Landing Page</h2>
-            </div>
-
-            <div className="flex gap-6 p-4">
-              <Image
-                src={'/programacao/Nextjs.svg'}
-                alt="Next Js"
-                width={36}
-                height={24}
-              />
-              <Image
-                src={'/programacao/Tailwind.svg'}
-                alt="Tailwind CSS"
-                width={36}
-                height={24}
-              />
-            </div>
-          </div>
-        </div>
+const Projetos = () => {
+  return (
+    <section
+      id="projetosSec"
+      className="hidden flex gap-12 flex-col justify-center items-center"
+    >
+      <ProjetosTitle />
+      <article className="md:grid md:grid-cols-2 xl:px-72 px-6 gap-12 grid grid-cols-1">
+        {projects.map((p) => (
+          <Card className="bg-amber-800/20 border-neutral-100/10" key={p.name}>
+            <CardHeader className="flex flex-col gap-1">
+              <CardTitle className="text-neutral-300">
+                <Link href={p.site ?? p.github}>{p.name}</Link>
+              </CardTitle>
+              <div className="flex flex-row gap-2">
+                {p.github && (
+                  <Link href={p.github}>
+                    <Badge className="md:text-xs text-[8px] cursor-pointer bg-neutral-800">
+                      GitHub
+                    </Badge>
+                  </Link>
+                )}
+                {p.site && (
+                  <Link href={p.site}>
+                    <Badge className="cursor-pointer bg-neutral-800">
+                      Ver mais
+                    </Badge>
+                  </Link>
+                )}
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p className="text-neutral-400">{p.description}</p>
+            </CardContent>
+          </Card>
+        ))}
       </article>
     </section>
   )
